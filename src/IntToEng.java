388da989ce5fee5e34a021f45ff5e056be49bc37
ErikaTai <g12951te@gm.tsuda.ac.jp>
2015-05-20 10:43:47 +0900

    119,1019,151で成功

--- src/IntToEng.java
+++ src/IntToEng.java
@@ -19,27 +19,60 @@ public class IntToEng {
     	String[] ENG_LIST3={"0","0","0","hundred","thousand"};
 
     	if(input<100){
-    		if(input<20) {  //0〜19の場合は直接アクセス
-    			b=ENG_LIST[input];
-    		}else{  //20以上の場合
-    			if(remainder(input)==0) {  //1の位が0
-    				b=ENG_LIST2[divide(input)];
-    			}else{  //1の位が0以外
-    				b=ENG_LIST2[divide(input)]+" "+ENG_LIST[remainder(input)];
-    			}/*else{
-    			b=ENG_LIST[input];
-    			}*/
+    		b = zeroToNinetynine(input, ENG_LIST, ENG_LIST2);
+    	}else if(input<10000){  //100以上10000未満の場合
+    		b = divisible(input, ENG_LIST, ENG_LIST3);
+    		if(!isDivisible(input)) {
+    			int d = 1;
+    			for(int i=0; i<digit(input)-1; i++) {
+    				d *= 10;
+    			}
+    			input = remainder(input,d);
+    			if(input<100) {
+    				b += " "+zeroToNinetynine(input, ENG_LIST, ENG_LIST2);
+    			}else{
+    				
+    			}
     		}
-    	}else{
-    		b=ENG_LIST[msd(input)]+" "
-    				+ENG_LIST3[digit(input)];
     	}
     	return b;
     }
+
+	public static String zeroToNinetynine(int input, String[] ENG_LIST,
+			String[] ENG_LIST2) {
+		String b;
+		if(input<20) {  //0〜19の場合は直接アクセス
+			b=ENG_LIST[input];
+		}else{  //20以上の場合
+			if(remainder(input)==0) {  //1の位が0
+				b=ENG_LIST2[divide(input)];
+			}else{  //1の位が0以外
+				b=ENG_LIST2[divide(input)]+" "+ENG_LIST[remainder(input)];
+			}
+		}
+		return b;
+	}
+    
+    public static boolean isDivisible(int input) {  //キリのいい数字かどうか
+    	input %= 10;
+    	if(input>10) isDivisible(input);
+    	return input==0;
+    }
+
+	public static String divisible(int input, String[] ENG_LIST,
+			String[] ENG_LIST3) {
+		String b;
+		b=ENG_LIST[msd(input)]+" "+ENG_LIST3[digit(input)];
+		return b;
+	}
     
     static int remainder(int input){
+    	return remainder(input,10);
+    }
+    
+    static int remainder(int input, int n) {
     	int remain;
-    	remain=input%10;
+    	remain=input%n;
     	if(remain>=10) remainder(remain);
     	return remain;
     }
